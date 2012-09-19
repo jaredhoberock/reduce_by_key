651dc9d41cef79c3478d173f8875882896615120
Jared Hoberock <jaredhoberock@gmail.com>
2012-09-19 12:21:46 -0700

    Eliminate a typo and test that everything's working.

--- reduce_by_key.cpp
+++ reduce_by_key.cpp
@@ -4,10 +4,12 @@
 #include <thrust/system/cpp/memory.h>
 #include <thrust/unique.h>
 #include <thrust/reduce.h>
+#include <thrust/random.h>
 #include <vector>
 #include <iterator>
 #include <tbb/blocked_range.h>
 #include <tbb/parallel_for.h>
+#include <tbb/tbb_thread.h>
 #include <iostream>
 #include <cassert>
 #include "head_flags.hpp"
@@ -166,7 +168,7 @@ template<typename Iterator1, typename Iterator2, typename Iterator3, typename It
   size_t interval_size;
   size_t num_intervals;
 
-  serial_reduce_by_key_body(Iterator1 keys_first, Iterator2 values_first, Iterator3 result_offset, Iterator4 keys_result, Iterator5 values_result, Iterator6 carry_result_key, size_t n, size_t interval_size, size_t num_intervals)
+  serial_reduce_by_key_body(Iterator1 keys_first, Iterator2 values_first, Iterator3 result_offset, Iterator4 keys_result, Iterator5 values_result, Iterator6 carry_result, size_t n, size_t interval_size, size_t num_intervals)
     : keys_first(keys_first), values_first(values_first), result_offset(result_offset), keys_result(keys_result), values_result(values_result), carry_result(carry_result), n(n), interval_size(interval_size), num_intervals(num_intervals)
   {}
 
@@ -241,27 +243,28 @@ template<typename Iterator1, typename Iterator2, typename Iterator3, typename It
                   Iterator3 keys_result,
                   Iterator4 values_result)
 {
-  // assume a dual core
-  size_t p = 2;
+  // count the number of processors
+  const size_t p = tbb::tbb_thread::hardware_concurrency();
 
   // generate O(P) intervals of sequential work
   typename thrust::iterator_difference<Iterator1>::type n = keys_last - keys_first;
   size_t interval_size = n / p;
   size_t num_intervals = divide_ri(n, interval_size);
 
-  std::cout << "reduce_by_key(): interval_size: " << interval_size << std::endl;
+  //std::clog << "reduce_by_key(): interval_size: " << interval_size << std::endl;
+  //std::clog << "reduce_by_key(): num_intervals: " << num_intervals << std::endl;
 
   // decompose the input into intervals of size N / num_intervals
   // add one extra element to this vector to store the size of the entire result
   std::vector<size_t> interval_output_offsets(num_intervals + 1, 0);
-  auto tail_flags = make_tail_flags(keys_first, keys_last);
 
   // first count the number of tail flags in each interval
+  auto tail_flags = make_tail_flags(keys_first, keys_last);
   reduce_intervals(tail_flags.begin(), tail_flags.end(), interval_size, interval_output_offsets.begin() + 1, thrust::plus<size_t>());
 
-  std::cout << "reduce_by_key(): num_tail_flags_per_interval: ";
-  std::copy(interval_output_offsets.begin(), interval_output_offsets.end(), std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
+  //std::clog << "reduce_by_key(): num_tail_flags_per_interval: ";
+  //std::copy(interval_output_offsets.begin(), interval_output_offsets.end(), std::ostream_iterator<int>(std::clog, " "));
+  //std::clog << std::endl;
 
   // scan the counts to get each body's output offset
   thrust::cpp::tag seq;
@@ -269,9 +272,9 @@ template<typename Iterator1, typename Iterator2, typename Iterator3, typename It
                          interval_output_offsets.begin() + 1, interval_output_offsets.end(), 
                          interval_output_offsets.begin() + 1);
 
-  std::cout << "reduce_by_key(): interval_output_offsets: ";
-  std::copy(interval_output_offsets.begin(), interval_output_offsets.end(), std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
+  //std::clog << "reduce_by_key(): interval_output_offsets: ";
+  //std::copy(interval_output_offsets.begin(), interval_output_offsets.end(), std::ostream_iterator<int>(std::clog, " "));
+  //std::clog << std::endl;
 
   // do a reduce_by_key serially in each thread
   // the final interval never has a carry by definition
@@ -279,15 +282,15 @@ template<typename Iterator1, typename Iterator2, typename Iterator3, typename It
   tbb::parallel_for(::tbb::blocked_range<size_t>(0, num_intervals, 1),
     make_serial_reduce_by_key_body(keys_first, values_first, interval_output_offsets.begin(), keys_result, values_result, carry_value.begin(), n, interval_size, num_intervals));
 
-  std::cout << "reduce_by_key(): carry values: ";
-  std::copy(carry_value.begin(), carry_value.end(), std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
+  //std::clog << "reduce_by_key(): carry values: ";
+  //std::copy(carry_value.begin(), carry_value.end(), std::ostream_iterator<int>(std::clog, " "));
+  //std::clog << std::endl;
 
   size_t size_of_result = interval_output_offsets.back();
 
-  std::cout << "reduce_by_key(): partial values: ";
-  std::copy(values_result, values_result + size_of_result, std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
+  //std::clog << "reduce_by_key(): partial values: ";
+  //std::copy(values_result, values_result + size_of_result, std::ostream_iterator<int>(std::clog, " "));
+  //std::clog << std::endl;
 
   // accumulate the carry values
   // note that the last interval does not have a carry
@@ -310,37 +313,54 @@ template<typename Iterator1, typename Iterator2, typename Iterator3, typename It
 template<typename Iterator1, typename Iterator2>
   void generate_test_case(Iterator1 keys_first, Iterator1 keys_last, Iterator2 values_first)
 {
-  size_t segment_size = 2;
-
   size_t n = keys_last - keys_first;
 
-  for(int i = 0; i < n; ++i)
+  size_t entropy = 3;
+
+  thrust::default_random_engine rng(13);
+  for(size_t i = 0, k = 0; i < n; ++i)
   {
-    keys_first[i] = i / segment_size;
-    values_first[i] = 1;
+    keys_first[i] = k;
+    if(rng() % entropy == 0)
+      k++;
+  }
+
+  for(size_t i = 0; i < n; ++i)
+  {
+    values_first[i] = rng() % entropy;
   }
 }
 
 
 int main()
 {
-  size_t n = 13;
+  //size_t n = 13;
+  size_t n = 100000000;
   std::vector<int> keys(n);
   std::vector<int> values(n, 1);
+
+  std::cout << "generating test case... ";
   generate_test_case(keys.begin(), keys.end(), values.begin());
+  std::cout << "done." << std::endl;
 
-  std::cout << "main(): keys      : ";
-  std::copy(keys.begin(), keys.end(), std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
+  // silence for large n
+  if(n < 100)
+  {
+    std::clog.rdbuf((n < 100) ? std::clog.rdbuf() : 0);
 
-  std::cout << "main(): tail_flags: ";
-  auto flags = make_tail_flags(keys.begin(), keys.end());
-  std::copy(flags.begin(), flags.end(), std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
+    std::clog << "main(): keys      : ";
+    std::copy(keys.begin(), keys.end(), std::ostream_iterator<int>(std::clog, " "));
+    std::clog << std::endl;
 
-  std::cout << "main(): values: ";
-  std::copy(values.begin(), values.end(), std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
+    std::clog << "main(): tail_flags: ";
+    auto flags = make_tail_flags(keys.begin(), keys.end());
+    std::copy(flags.begin(), flags.end(), std::ostream_iterator<int>(std::clog, " "));
+    std::clog << std::endl;
+
+    std::clog << "main(): values: ";
+    std::copy(values.begin(), values.end(), std::ostream_iterator<int>(std::clog, " "));
+    std::clog << std::endl;
+  }
 
   std::vector<int> keys_result(n);
   std::vector<int> values_result(n, 13);
@@ -349,13 +369,16 @@ int main()
   keys_result.erase(ends.first, keys_result.end());
   values_result.erase(ends.second, values_result.end());
 
-  std::cout << "main(): keys_result: ";
-  std::copy(keys_result.begin(), keys_result.end(), std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
+  if(n < 100)
+  {
+    std::clog << "main(): keys_result: ";
+    std::copy(keys_result.begin(), keys_result.end(), std::ostream_iterator<int>(std::clog, " "));
+    std::clog << std::endl;
 
-  std::cout << "main(): values_result: ";
-  std::copy(values_result.begin(), values_result.end(), std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
+    std::clog << "main(): values_result: ";
+    std::copy(values_result.begin(), values_result.end(), std::ostream_iterator<int>(std::clog, " "));
+    std::clog << std::endl;
+  }
 
   std::vector<int> keys_result_reference(n);
   std::vector<int> values_result_reference(n);
@@ -363,16 +386,21 @@ int main()
   keys_result_reference.erase(ends1.first, keys_result_reference.end());
   values_result_reference.erase(ends1.second, values_result_reference.end());
 
-  std::cout << "main(): keys_result_reference: ";
-  std::copy(keys_result_reference.begin(), keys_result_reference.end(), std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
+  if(n < 100)
+  {
+    std::clog << "main(): keys_result_reference: ";
+    std::copy(keys_result_reference.begin(), keys_result_reference.end(), std::ostream_iterator<int>(std::clog, " "));
+    std::clog << std::endl;
 
-  std::cout << "main(): values_result_reference: ";
-  std::copy(values_result_reference.begin(), values_result_reference.end(), std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
+    std::clog << "main(): values_result_reference: ";
+    std::copy(values_result_reference.begin(), values_result_reference.end(), std::ostream_iterator<int>(std::clog, " "));
+    std::clog << std::endl;
+  }
 
   assert(keys_result_reference == keys_result);
   assert(values_result_reference == values_result);
+
+  std::cout << "test passed." << std::endl;
 
   return 0;
 }
